eae2b7d85502e224f90c8ce07c9aadb855a35c04
AnandSGit <5903620+AnandSGit@users.noreply.github.com>
2021-03-16 20:00:22 -0700

    Update hybrid_rabbit.go

--- hybrid_rabbit.go
+++ hybrid_rabbit.go
@@ -8,21 +8,14 @@ import (
 
 // RabbitPacket - Please refer KafkaPacket
 type RabbitPacket struct {
-	// HandleConn holds the connection object to RabbitMQ Server
 	HandleConn *ampq.Connection
-	// RChannel holds the RabbitMQ Connection Object locally. Single connection would
-	// be used for handling all the connections from any single process that includes
-	// responding for requests.
 	RChannel *ampq.Channel
 }
 
-// Connect defines the connection procedure for RabbitMQ Server. As part
-// of Connect procedure we would be reading the RabbitMQ configuration from the
-// Config handler and would dial the connection towards RabbitMQ Server.
+// Connect defines the connection procedure for RabbitMQ Server.
 func (rp *RabbitPacket) Connect() error {
-	var e error
-	// Get the Connection Handle to RabbitMQ and Store in Packet Object
-	rp.HandleConn, e = ampq.Dial(HPipeConfig.RServerPort)
+
+	rp.HandleConn, e := ampq.Dial(HPipeConfig.RServerPort)
 	if e != nil {
 		log.Printf("%v", e)
 		return e
@@ -30,26 +23,21 @@ func (rp *RabbitPacket) Connect() error {
 	return nil
 }
 
-// Dispatch defines the Produce or Publisher Function for RabbitMQ Medium. User just
-// needs to pass to which Pipe message needs to be passed and Message itself.
+// Dispatch defines the Produce or Publisher Function for RabbitMQ Medium.
 func (rp *RabbitPacket) Dispatch(pipe string, d interface{}) error {
 	var q ampq.Queue
 	var e error
 
-	// Get Channel object for RabbitMQ Connection. Ignoring the errors for now.
 	rp.RChannel, _ = rp.HandleConn.Channel()
-	// Declare the Queue based on Pipe name provided for this API
 	if q, e = rp.RChannel.QueueDeclare(pipe, false, false, false, false, nil); e != nil {
 		log.Printf("%v", e)
 		return e
 	}
-	// Encode the message before appending into KAFKA Message struct
 	b, ei := Encode(d)
 	if ei != nil {
 		log.Printf("%v", ei)
 		return ei
 	}
-	// Send the message to the specified Pipe
 	e = rp.RChannel.Publish("", q.Name, false, false, ampq.Publishing{
 		ContentType: "text/plain",
 		Body:        b,
@@ -57,30 +45,25 @@ func (rp *RabbitPacket) Dispatch(pipe string, d interface{}) error {
 	return nil
 }
 
-// Accept defines the Subscription / Consume procedure. Again same connection would
-// be used for handling all the communication with RabbitMQ as it is goroutine safe.
+// Accept defines the Subscription / Consume procedure.
 func (rp *RabbitPacket) Accept(pipe string, fn Process) error {
+	
 	var q ampq.Queue
-	var e error
 
-	// Getting the Consume Channel
-	rp.RChannel, e = rp.HandleConn.Channel()
+	rp.RChannel, e := rp.HandleConn.Channel()
 	if e != nil {
 		log.Printf("%v", e)
 		return e
 	}
-	// Define the Queue with Pipe name passed.
 	if q, e = rp.RChannel.QueueDeclare(pipe, false, false, false, false, nil); e != nil {
 		log.Printf("%v", e)
 		return e
 	}
-	// Initiate the consume channel. Returned "m" is channel.
 	m, ei := rp.RChannel.Consume(q.Name, pipe, true, false, false, false, nil)
 	if ei != nil {
 		log.Printf("%v", e)
 		return e
 	}
-	// Initiate a infinite loop in reading incoming messages.
 	inFinity := make(chan bool)
 	go func() {
 		for ms := range m {
@@ -106,9 +89,7 @@ func (rp *RabbitPacket) Remove(pipe string) error {
 	return nil
 }
 
-// Close will close RabbitMQ connection. After this call, this Object will
-// become un-usable. Unexpected behavior will occur if user tries to use
-// the NPacket object post "Disconnect" call.
+// Close will close RabbitMQ connection.
 func (rp *RabbitPacket) Close() {
 	rp.HandleConn.Close()
 }
